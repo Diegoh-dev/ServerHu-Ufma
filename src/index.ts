ebcca83f3d361959f64b245820821d4892b6e186
Diego ribeiro <Diegoh.developer@gmail.com>
2024-01-31 23:31:18 -0300

    feat: Iniciando rota de autenticação.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-const express = require('express');
+import express, { Request, Response, json } from 'express';
 
 const app = express();
 
 const port = 8081;
 
+app.use(express.json());
 
 
-app.listen(port,()=>{
-    console.log('Servidor rodando! 🚀')
+app.post('/users/auth',(req:Request,res:Response) => {
+    const {prontuario,sus} = req.body;
+
+    console.log({prontuario})
+    console.log({sus})
+
+   return res.send("ok");
 })
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port} 🚀`);
+});
+
+/*
+{
+    "prontuario": 16081999,
+    "sus": "55555555"
+}
+*/
